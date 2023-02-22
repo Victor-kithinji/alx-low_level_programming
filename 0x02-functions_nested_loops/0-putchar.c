49fe5ace35a5d38ddcc0b98e975be649062fe86b
Victor-kithinji <122810077+Victor-kithinji@users.noreply.github.com>
2023-02-22 09:39:35 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,15 @@
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-void print_alphabet(void)
+int main()
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++){
+		
 		_putchar(letter);
+	}
 
 	_putchar('\n');
+	return 0;
 }
