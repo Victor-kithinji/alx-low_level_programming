515aca3814c3326728c655a2c9cb6bfac8d3e74e
Victor-kithinji <122810077+Victor-kithinji@users.noreply.github.com>
2023-02-28 14:11:26 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,23 +6,27 @@
   * Return: void
   */
 void rev_string(char *s)
-{char ch, *p, *q;
+{
+	 int i, c, k;
+	char *a, aux;
 
-  /* Point q at the last character in s[], or at &s[0] if s is empty. */
-  for (q = s; *q != '\0'; ++q)
-    ;
-  if (q > s)
-    --q;
-  
-  /* Simultaneously sweep p from front to back and q from back to front,
-   * swapping characters at p and q, until the pointers meet. */
-  for (p = s; p < q; ++p, --q) {
-    ch = *p;
-    *p = *q;
-    *q = ch;
-  }
+	a = s;
 
-  return s;
-}
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = 1; k < c; k++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		aux = s[i];
+		s[i] = *a;
+		*a = aux;
+		a--;
 	}
 }
