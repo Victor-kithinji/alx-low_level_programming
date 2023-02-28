98e594f8ad9368085012921f0c471092b41d543a
Victor-kithinji <kithinjivictor69@gmail.com>
2023-02-28 01:15:51 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Write a function that swaps the values of two integers.
+*/
+void swap_int(int *a, int *b);
+{
+	int a=98;
+	int b=42;
+	temp=a;
+	a=b;
+	b=temp;
+	print ("%d swap a and b", a,b);
+}
