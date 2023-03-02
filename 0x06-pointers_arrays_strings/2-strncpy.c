25b9f0fa85ef066cc3ff7c170bcad614a1a06b6e
Victor-kithinji <122810077+Victor-kithinji@users.noreply.github.com>
2023-03-02 18:13:30 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-/**_strncpy: copies the strings
- * dest: string
- * src: string
+/**
+ * _strncpy - Copy a string starting from index 0 of `dest`.
+ * @dest: string
+ * @src: string
  * @n: number of chars to copy over
- * return: 'dest' edited string
+ * Return: `dest` edited string
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 
