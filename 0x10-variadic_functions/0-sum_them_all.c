fdbc109941bfeee2489f842ff1090c7e5a384d91
Victor-kithinji <122810077+Victor-kithinji@users.noreply.github.com>
2023-03-23 13:04:47 +0300

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,27 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
+
 /**
- * sum_them_all - sums all arguments passed
+ * sum_them_all - that returns the sum of all its parameters.
+ * @n: no of arguments
  *
- * @n: number of arguments
- *
- * Return: sum of arguments
+ * Return: sum of the parameters, 0 if no parameter
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    va_start(args, n);
-	if (n == 0){return (0);}
+	unsigned int i;
+	int sum = 0;
+	va_list list;
 
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += va_arg(args, int);
-    }
+	if (n == 0)
+		return (0);
 
-    va_end(args);
-    return sum;
+	va_start(list, n); /* initialize argument lisy */
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int); /* get next argument value */
+
+	va_end(list); /* clean_up */
+
+	return (sum);
 }
